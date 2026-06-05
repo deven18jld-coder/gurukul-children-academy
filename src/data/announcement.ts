236ea3ac9cf48dba38418b56d82de06b06e1346e
Deven Chauhan <deven.18jld@gmail.com>
2026-06-05 17:34:31 +0530

    fourth commit

---
+++ src/data/announcement.ts
@@ -0,0 +1,9 @@
+export const announcementData = {
+  showAnnouncement: true,
+  sessionYear: "2026–2027",
+  status: "Admissions Open",
+  seatsMessage: "Limited Seats Available",
+  classesMessage: "Nursery to Class 8 Admissions Ongoing",
+  link: "/admissions#admissions-form",
+  buttonText: "Apply Now"
+};
